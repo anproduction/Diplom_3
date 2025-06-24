568592078162265c6091bba50d4dbca7a0a25e96
Anastasia Prodan <prodananastasija@yandex.com>
2025-06-24 23:02:34 +0300

    fix_locators

--- locators.py
+++ locators.py
@@ -10,11 +10,11 @@ class MainPageLocators:
     READY_TEXT = (By.XPATH, "//p[text()='Готовы:']")  # Текст "Готовы"
     BUN_INGREDIENT = (By.XPATH, "//img[@alt='Флюоресцентная булка R2-D3']")  # Кнопка "Флюоресцентная булка"
     INGREDIENT_DETAILS = (By.XPATH, "//h2[text()='Детали ингредиента']")  # Поп-ап "Детали ингредиента")
-    CLOSE_POP_UP_INGREDIENT_DETAILS_BUTTON = (By.XPATH, "(//button[contains(@class, 'close_modified')])[1]")  # Кнопка "Закрыть" на поп-апе Детали ингредиента
+    CLOSE_POP_UP_INGREDIENT_DETAILS_BUTTON = (By.CSS_SELECTOR, "button[class*='modal__close']")  # Кнопка "Закрыть" на поп-апе Детали ингредиента
     SAUCES_BUTTON = (By.XPATH, "//h2[text()='Соусы']")  # Кнопка "Соусы"
     SPICY_SAUCE = (By.XPATH, "//img[@alt='Соус Spicy-X']")  # Соус Spicy-X
     CONSTRUCTOR_FIELD = (By.XPATH, "//span[text()='Перетяните булочку сюда (верх)']")  # Поле для создания бургера
-    COUNTER = (By.XPATH, "(//p[contains(@class, 'counter_counter__num__3nue1')])[3]") # Счетчик в правом верхнем углу над булкой
+    COUNTER = (By.XPATH, "//a[.//img[@alt='Флюоресцентная булка R2-D3']]//div[contains(@class, 'counter_counter')]//p[contains(@class, 'counter_counter__num')]")  # Счетчик в правом верхнем углу над булкой
     ORDER_ID = (By.XPATH, "//p[text()='идентификатор заказа']")  # Идентификатор заказа
     ORDER_POP_UP = (By.XPATH, "//div[contains(@class, 'Modal_modal__contentBox')]")  # Поп-ап заказа
     CLOSE_POP_UP_ORDER = (By.XPATH, "//button[contains(@class, 'Modal_modal__close_modified__3V5XS')]") # Кнопка "Закрыть" поп-ап
@@ -22,8 +22,8 @@ class MainPageLocators:
 class OrderPageLocators:
     ORDER_LIST = (By.XPATH, "(//div[contains(@class, 'OrderHistory_dataBox__1mkxK')])[1]")  # Первый элемент списка заказов
     STRUCTURE_TEXT = (By.XPATH, "//p[text()='Cостав']")  # Текст "Состав"
-    ALL_ORDERS_COUNTER = (By.XPATH, "(//p[contains(@class, 'OrderFeed_number')])[1]")  # Общее кол-во заказов
-    TODAY_ORDERS_COUNTER = (By.XPATH, "(//p[contains(@class, 'OrderFeed_number')])[2]")  # Выполнено за сегодня
+    ALL_ORDERS_COUNTER = (By.XPATH, '//p[text()="Выполнено за все время:"]/following-sibling::p')  # Общее кол-во заказов
+    TODAY_ORDERS_COUNTER = (By.XPATH, '//p[text()="Выполнено за сегодня:"]/following-sibling::p')  # Выполнено за сегодня
 
 class AccountPageLocators:
     INPUT_EMAIL = (By.XPATH, "//label[text()='Email']/../input")  # Ввод Email
