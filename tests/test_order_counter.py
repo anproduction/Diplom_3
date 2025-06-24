9e6974531e8edc519323d9203643dd60477b4443
Anastasia Prodan <prodananastasija@yandex.com>
2025-06-25 00:02:17 +0300

    fix_test_order_counter

--- tests/test_order_counter.py
+++ tests/test_order_counter.py
@@ -6,8 +6,10 @@ class TestOrderCounter:
     @allure.story("Проверка счётчиков заказов")
     @allure.title("Проверяем, что счётчики 'Выполнено за всё время' и 'Выполнено за сегодня' отображаются")
     def test_order_counters_visibility(self, main_page, feed_page):
-        total = feed_page.get_total_orders_count()
-        today = feed_page.get_today_orders_count()
+        counters = {
+            "Общее количество заказов": feed_page.get_total_orders_count(),
+            "Количество заказов за сегодня": feed_page.get_today_orders_count()
+        }
 
-        assert total is not None and total >= 0, "Общее количество заказов отсутствует или меньше 0"
-        assert today is not None and today >= 0, "Количество заказов за сегодня отсутствует или меньше 0"
+        for name, value in counters.items():
+            assert value is not None and value >= 0, f"{name} отсутствует или меньше 0"
